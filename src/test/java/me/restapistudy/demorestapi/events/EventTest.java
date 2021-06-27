e2b8b206ae64fe11e0713447c7fed8ac597dc3c6
pelexsa <pelexsa@naver.com>
2021-06-28 00:15:51 +0900

    Event domain

---
+++ src/test/java/me/restapistudy/demorestapi/events/EventTest.java
@@ -0,0 +1,34 @@
+package me.restapistudy.demorestapi.events;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Inflearn")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean(){
+        // Given
+        String name = "Event";
+        String description = "Spring";
+
+        // When
+        Event event = new Event();
+        event.setName("Event");
+        event.setDescription("Spring");
+
+        // Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+}
