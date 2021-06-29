81c4932da998ba60fdf8b8898145bef216aa761d
pelexsa <pelexsa@naver.com>
2021-06-29 16:20:36 +0900

    Add test description annotation

--- src/test/java/me/restapistudy/demorestapi/events/EventControllerTests.java
+++ src/test/java/me/restapistudy/demorestapi/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package me.restapistudy.demorestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.restapistudy.demorestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -36,6 +37,7 @@ public class EventControllerTests {
 
     // api.json api.xml
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -65,7 +67,8 @@ public class EventControllerTests {
     }
 
     @Test
-    public void create_Bad_Request() throws Exception {
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
+    public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -92,6 +95,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto =  EventDto.builder().build();
 
@@ -100,6 +104,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void create_Bad_Request_Wrong_Input() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -108,7 +113,7 @@ public class EventControllerTests {
                 .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
                 .beginEventDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
                 .endEventDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
-                .basePrice(10000)
+                .basePrice(10000 )
                 .maxPrice(200)
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타텁 팩토리")
