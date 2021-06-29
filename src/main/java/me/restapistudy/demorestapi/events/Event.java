39550d16f19bbf6421cd74270227b6b694fa31d2
pelexsa <pelexsa@naver.com>
2021-06-29 09:32:43 +0900

    POST /api/events returns 201

--- src/main/java/me/restapistudy/demorestapi/events/Event.java
+++ src/main/java/me/restapistudy/demorestapi/events/Event.java
@@ -2,12 +2,15 @@ package me.restapistudy.demorestapi.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
 
+    @Id @GeneratedValue
     private Integer id;
     private String name;
     private String description;
@@ -21,6 +24,6 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
-
 }
---
+++ src/main/java/me/restapistudy/demorestapi/events/EventRepository.java
@@ -0,0 +1,7 @@
+package me.restapistudy.demorestapi.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+
+}
